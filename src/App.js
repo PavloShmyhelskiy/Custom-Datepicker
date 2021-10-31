9286b3e962b1036db7c030059f381f807a8ed402
Pavlo Shmygelskyi <pavlo.shmygelskyi@gmail.com>
2021-10-31 14:59:22 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import {useState } from "react";
 import DatePicker from "./DatePicker/DatePicker";
-
+import './App.css';
 
 function App() {
   const [date, setDate] = useState([[new Date(new Date() - 24 * 3600 * 5 * 1000), new Date()]])
 
   return (
-    <DatePicker type="multiRange" value={date} onChange={setDate} />
+    <div  className="datePicker">
+      <DatePicker type="multiRange" value={date} onChange={setDate} />
+    </div>
   );
 }
 
